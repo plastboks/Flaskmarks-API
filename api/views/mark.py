9e02847e75c6e11a02b9a20403347d4599b90029
Alexander Skjolden <alex@plastboks.net>
2014-06-30 20:57:55 +0200

    pep8
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- api/views/mark.py
+++ api/views/mark.py
@@ -3,9 +3,11 @@
 
 from ..core.setup import Resource
 
+
 class Mark(Resource):
     def get(self, todo_id):
         return {todo_id: "test"}
+
 
 class Marks(Resource):
     def get(self, todo_id):
