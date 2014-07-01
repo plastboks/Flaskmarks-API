8537875831834ae8e2ba81c09056aedc97e5f4e1
Alexander Skjolden <alex@plastboks.net>
2014-07-01 20:59:23 +0200

    some basic auth...
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- api/views/mark.py
+++ api/views/mark.py
@@ -1,14 +1,16 @@
 # api/views/mark.py
 
 
-from ..core.setup import Resource
+from ..core.setup import Resource, auth
 
 
 class Mark(Resource):
+    @auth.login_required
     def get(self, todo_id):
         return {todo_id: "test"}
 
 
 class Marks(Resource):
+    @auth.login_required
     def get(self, todo_id):
         return {todo_id: "test"}
